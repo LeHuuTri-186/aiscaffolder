62448bb3877230f8cc126562dcd2e8e917a8491b
LeHuuTri-186 <110376741+LeHuuTri-186@users.noreply.github.com>
2024-11-26 20:47:58 +0700

    first commit: project initialization

---
+++ src/main/java/com/initializer/aiscaffolder/AiscaffolderApplication.java
@@ -0,0 +1,13 @@
+package com.initializer.aiscaffolder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiscaffolderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiscaffolderApplication.class, args);
+	}
+
+}
