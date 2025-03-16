4b8648e28851041b6ca8dde52bbb24d183beef66
LeHuuTri-186 <110376741+LeHuuTri-186@users.noreply.github.com>
2025-03-16 15:11:30 +0700

    [FIX bug] Enhance entity generation and Payload

--- modules/project-template-engine/src/main/java/com/aiscaffolder/projecttemplateengine/application/services/GenerateProjectService.java
+++ modules/project-template-engine/src/main/java/com/aiscaffolder/projecttemplateengine/application/services/GenerateProjectService.java
@@ -2,12 +2,13 @@ package com.aiscaffolder.projecttemplateengine.application.services;
 
 import com.aiscaffolder.projecttemplateengine.domain.entities.Configuration;
 import com.aiscaffolder.projecttemplateengine.domain.entities.Entity;
+import com.aiscaffolder.projecttemplateengine.domain.entities.Relationship;
 
 import java.util.List;
 
 public interface GenerateProjectService {
     void generateProject();
     void generateWrapper();
-    void generateEntities(List<Entity> entities, Configuration configuration, String outputDir);
+    void generateEntities(List<Entity> entities, Configuration configuration, List<Relationship> relationships, String outputDir);
     void generateConfigurations(Configuration configuration, String outputDir);
 }
